ce4dd8225596a1d364b41e148e1b4bd49e0e23ff
Ahmed Eldabaa <Ahmed.Eldabaa@gds-direct.com>
2025-09-09 15:38:00 +0300

    feat(available-places): fetching data using the injected HttpClient

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,16 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-available-places',
   standalone: true,
   templateUrl: './available-places.component.html',
   styleUrl: './available-places.component.css',
-  imports: [PlacesComponent, PlacesContainerComponent],
+  imports: [PlacesComponent, PlacesContainerComponent, HttpClientModule],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places').subscribe({
+      next: (resData) => {
+        console.log(resData.places);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
